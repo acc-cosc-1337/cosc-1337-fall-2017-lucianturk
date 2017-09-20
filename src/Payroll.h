c82b155802eaae32a921874bfdccc6fa926b7503
lucianturk <31940651+lucianturk@users.noreply.github.com>
2017-09-20 18:44:22 -0500

    initial commit for function def

--- src/Payroll.h
+++ src/Payroll.h
@@ -1,7 +1,12 @@
 #ifndef _PAYROLL_H
 #define _PAYROLL_H
 
-
+double get_reg_hours(double total_hours);
+double get_ot_hours(double total_hours);
+double get_gross_pay(double reg_hours, double ot_hours, double hourly_rate);
+double get_gross_pay(double salary);
+void show_check_hourly(double gross_pay, double reg_hours, double ot_hours);
+void show_check_salary(double gross_pay);
 
 
 #endif

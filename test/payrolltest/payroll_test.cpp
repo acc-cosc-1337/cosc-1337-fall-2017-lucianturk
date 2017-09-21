5027c4e1c88474a062da6c27abf280edd568b699
lucianturk <31940651+lucianturk@users.noreply.github.com>
2017-09-20 19:12:05 -0500

    initial commit for unit tests

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -1,1 +1,12 @@
+#include "gtest/gtest.h"
+#include "Payroll.h"
 
+TEST(payroll_tests, when_hours_greater_than_40){
+    EXPECT_EQ(40, get_reg_hours(50));
+}
+
+TEST(payroll_tests, when_hours_less_than_0){
+    EXPECT_EQ(0, get_reg_hours(-10));
+}
+
+#EXPECT_EQ(expectedValue, actualValue);

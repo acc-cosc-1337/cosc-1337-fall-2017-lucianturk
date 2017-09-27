91f27c686c5529ca7bbfc28781b795e014428461
lucianturk <31940651+lucianturk@users.noreply.github.com>
2017-09-27 18:49:18 -0500

    added unit test for when_hours_are_0 to get_reg_hours

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -9,6 +9,10 @@ TEST(payroll_tests, when_hours_less_than_0){
     EXPECT_EQ(0, get_reg_hours(-10));
 }
 
+TEST(payroll_tests, when_hours_are_0){
+    EXPECT_EQ(0, get_reg_hours(0));
+}
+
 TEST(ot_hours, when_total_hours_greater_than_standard_hours){
     EXPECT_EQ(10, get_ot_hours(50));
 }

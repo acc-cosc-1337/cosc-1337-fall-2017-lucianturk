6c3d3ae7247c93d83ebb9c93131202d510723f96
lucianturk <31940651+lucianturk@users.noreply.github.com>
2017-09-27 18:26:22 -0500

    added unit test for ot_hours when total hours greater than standard hours

--- test/payrolltest/payroll_test.cpp
+++ test/payrolltest/payroll_test.cpp
@@ -9,4 +9,10 @@ TEST(payroll_tests, when_hours_less_than_0){
     EXPECT_EQ(0, get_reg_hours(-10));
 }
 
+TEST(ot_hours, when_total_hours_greater_than_standard_hours){
+    EXPECT_EQ(10, get_ot_hours(50));
+}
+
+
+
 
